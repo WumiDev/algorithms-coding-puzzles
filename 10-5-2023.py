cd055f4324d9bce2f0268953d54b34314dac3985
WumiDev <wumi2021@gmail.com>
2023-05-15 01:26:36 +0000

    modify: renamed workers to worker_efficiencies

--- 10-5-2023.py
+++ 10-5-2023.py
@@ -1,12 +1,12 @@
 from itertools import combinations
 
 def find_group_efficiency(worker_efficiencies):
-    num_workers = len(workers)
+    num_workers = len(worker_efficiencies)
     min_task_cost = float('inf')
 
     for i in range(num_workers):
-        excluded_worker = workers[i]
-        remaining_workers = workers[:i] + workers[i+1:]
+        excluded_worker = worker_efficiencies[i]
+        remaining_workers = worker_efficiencies[:i] + worker_efficiencies[i+1:]
         num_pairs = len(remaining_workers) // 2
         all_pairs = list(combinations(remaining_workers, 2))
         pair_costs = [abs(pair[0]-pair[1]) for pair in all_pairs]
@@ -20,7 +20,7 @@ def find_group_efficiency(worker_efficiencies):
         if best_pairs_cost < min_task_cost:
             min_task_cost = best_pairs_cost
             
-    print(f"\nMinimum cost: {min_task_cost}")
+    # print(f"\nMinimum cost: {min_task_cost}")
     
     return min_task_cost
 
@@ -29,6 +29,6 @@ def find_group_efficiency(worker_efficiencies):
 # worker_efficiencies = [4,2,8,1,9]
 worker_efficiencies = [1, 3, 54, 712, 52, 904, 113, 12, 135, 32, 31, 56, 23, 79, 611, 123, 677, 232, 997, 101, 111, 123, 2, 7, 24, 57, 99, 45, 666, 42, 104, 129, 554, 335, 876, 35, 15, 93, 13]
 min_cost = find_group_efficiency(worker_efficiencies)
-# print(f"Minimum cost: {min_cost}")
+print(f"Minimum cost: {min_cost}")
 
     
